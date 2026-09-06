5adae9cd1c14ebb9f5fe1ca0d19ac22486d4b681
Rajesh Mani <rajeshm79@outlook.com>
2026-09-06 15:07:12 +0530

    testcases changes

--- Tests/StudentAppBackendTests/StudentAppBackendTests.swift
+++ Tests/StudentAppBackendTests/StudentAppBackendTests.swift
@@ -21,7 +21,7 @@ struct StudentAppBackendTests {
     private func withApp(_ test: (Application) async throws -> ()) async throws {
         let app = try await Application.make(.testing)
         do {
-            try await configure(app)
+            try configure(app)
             try await app.autoMigrate()
             try await test(app)
             try await app.autoRevert()
